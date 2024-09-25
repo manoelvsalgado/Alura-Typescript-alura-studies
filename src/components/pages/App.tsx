f11bdbfc1e3de9f11759eb029201a7582ee40a2f
Manoel Salgado <manoelsalgadoneto@gmail.com>
2024-09-25 15:17:33 -0300

    estilizando componentes

---
+++ src/components/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Formulario from '../Formulario';
+import Lista from '../Lista';
+import './style.scss'
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
