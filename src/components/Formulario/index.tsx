d0e779092ee802cff8543147e1e72cce84fcce29
Manoel Salgado <manoelsalgadoneto@gmail.com>
2024-10-08 11:51:46 -0300

    04.Props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -31,7 +31,9 @@ class Formulario extends React.Component {
                         required
                     />
                 </div>
-                <Botao />
+                <Botao>
+                    Adicionar
+                </Botao>
             </form>
         )
     }
--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
 import style from './App.module.scss'
+import Cronometro from '../Cronometro';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
+      <Cronometro />
     </div>
   );
 }
