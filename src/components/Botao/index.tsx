15368e9c3a70ebe006b0d969fe9407227bc25986
Manoel Salgado <manoelsalgadoneto@gmail.com>
2024-10-10 13:53:11 -0300

    02. selecionando uma tarefa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,14 +1,17 @@
-import React, { Children } from "react";
-import style from './Botao.module.scss'
+import React from 'react';
+import style from './Botao.module.scss';
 
-class Botao extends React.Component <{children: React.ReactNode}> {
-    render() {
-        return(
-            <button className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-    }
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined
+}> {
+  render() {
+    const { type = "button" } = this.props;
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    )
+  }
 }
 
 export default Botao;
