5e1674c0b44ca784d788661dd96d26cb374e14bc
Manoel Salgado <manoelsalgadoneto@gmail.com>
2024-10-11 11:27:31 -0300

    07.Finalizando a aplicação

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,11 +6,17 @@ import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
-  const [selecionado, setSelecionado] = useState<ITarefa>();
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  ;
+  const [selecionado, setSelecionado] = 
+  useState<ITarefa>();
 
   function selecionaTarefa(tarefaSelecionada: ITarefa) {
     setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
   }
 
   return (
@@ -20,7 +26,7 @@ function App() {
         tarefas={tarefas}
         selecionaTarefa={selecionaTarefa}
         />
-      <Cronometro />
+      <Cronometro selecionado={selecionado}/>
     </div>
   );
 }
